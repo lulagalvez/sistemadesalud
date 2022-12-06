19d0762113bfeb2cb516e55cccb2c94a2ee99f3b
Lula Galvez <lulagalvez@github.com>
2022-12-06 11:14:26 -0300

    diagnostico medico

--- SistemaDeSalud/src/sistemadesalud/DiagnosticoMedico.java
+++ SistemaDeSalud/src/sistemadesalud/DiagnosticoMedico.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemadesalud;
 
-/**
- *
- * @author macta
- */
 public class DiagnosticoMedico {
-    
+    public DiagnosticoMedico() {
+        
+    }
 }
